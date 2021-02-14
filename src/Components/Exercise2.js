0a386e12b75831fa6b227466b386bf38ee840a96
Cierra-McDonald <cierralauren15@gmail.com>
2021-02-13 23:38:08 -0700

    exercise 2 finished

--- src/Components/Exercise2.js
+++ src/Components/Exercise2.js
@@ -3,31 +3,46 @@ import React, { Component } from 'react'
 export default class Exercise2 extends Component {
     state = {
 
-        input: ''
+        currenInput: '',
+        finalInput : ''
     
     }
 
-    handleInputChange = (userInput) => {
-        console.log('hello, you got this!')
+    handleInputChange = (e) => {
+        // console.log("hello")
+        console.log(e)
         this.setState({
-            input: userInput
-        })
+            currentInput: e.target.value
+        });
 
+    };
+
+    getState = () => {
+        
+        this.setState({finalInput : this.state.currentInput})
     }
-
 
     render() {
         return (
             <div>
                 <div className="exercise-box2">
                 Exercise 2: Create a component that will accept user input as a field when the user clicks a submit button populate that text in the component with Blue text.<br/>
+                <form>
+                    <input
+                    type="text"
+                    style={{color:"blue"}}
+                    onChange={this.handleInputChange}
+                    >
+                    </input><br/>
 
-                <input style={{color: this.state.input}}></input><br/>
-
-                <button
-                className="exercise-2-button"
-                onClick={() => this.handleInputChange()}>Submit!</button>
+                    <button className="exercise-2-button" type="button"
+                    onClick={this.getState}> 
+                    Submit!
+                    </button>
+                </form>
+                 <h3 style={{color:"blue"}}>Your string says: {this.state.finalInput}</h3>
                 </div>
+               
                 
             </div>
         )
