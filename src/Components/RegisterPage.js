e8b779b6d635fa5ee2c7224d7612e97c69d1153b
Cierra-McDonald <cierralauren15@gmail.com>
2021-02-15 18:06:37 -0700

    registe page finished

--- src/Components/RegisterPage.js
+++ src/Components/RegisterPage.js
@@ -22,14 +22,39 @@ export default class RegisterPage extends Component {
     }
     shipUserInfo=()=>{
 
-        const allUsers = JSON.parse(localStorage.getItem("USERS"))
-        allUsers[this.state.username] = this.state
-        localStorage.setItem("USERS", JSON.stringify(allUsers))
+        let allUsers = JSON.parse(localStorage.getItem("USERS")) //defining allUsers in localStorage and getting item "USERS"
+        
+        if (this.state.password !== this.state.reEnter){
+            // prompt the user to reenter the passwords
+            alert('Uh oh! Your passwords do not match, please try again.')
+            this.setState({
+                password: '',
+                reEnter: ''
+            })
+            return;
+        }
 
-        window.location= './signIn'
+        if (!allUsers) {//if there is no such thing as an allUsers object, set the item in local storage and create an empty object for the future users that will go in allUsers
+            localStorage.setItem("USERS", JSON.stringify({}))
+            allUsers = {}
+            allUsers[this.state.username] = this.state
+            localStorage.setItem("USERS", JSON.stringify(allUsers))
+            alert('Welcome! You are now registered!')
+            return window.location= './signIn'
+        }
 
+        if (allUsers[this.state.username]) {
+            alert('Uh oh! Someone already has this username, please make a new one.')
+            return window.location='./registerHere';
+        } else {
+            allUsers[this.state.username] = this.state
+            localStorage.setItem("USERS", JSON.stringify(allUsers))
+            alert('Welcome! You are now registered!')
+            window.location= './signIn'
+        }
 
     }
+
 
     render() {
         return (
@@ -39,8 +64,8 @@ export default class RegisterPage extends Component {
                     <input id="newName" type="text" placeholder="Enter Name" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
                     <input id ="username" type="text" placeholder="Create a Username" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
                     <input id ="email" type="text" placeholder="Enter Email" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
-                    <input id ="password" type="text" placeholder="Create New Password" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
-                    <input id ="reEnter" type="text" placeholder="Re-Enter Password" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
+                    <input id ="password" type="text" value={this.state.password} placeholder="Create New Password" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
+                    <input id ="reEnter" type="text" value={this.state.reEnter} placeholder="Re-Enter Password" className="input-boxes" onChange={(e)=> this.getUserInfo(e)}></input><br/>
                     <select id="security_q"className="select-box"
                     onChange={(e)=> this.getUserInfo(e)}>
                         <option>What city were you born in?</option>
